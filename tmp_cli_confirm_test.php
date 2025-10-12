1b19db934a13c01ba8c723fb8840cd078f26c1ff
Braian <braianoquen@gmail.com>
2025-10-12 14:55:54 -0500

    cambios

--- tmp_cli_confirm_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Temporal test runner
-$_SERVER['HTTP_HOST']='localhost';
-$_SERVER['REQUEST_SCHEME']='http';
-if (session_status()===PHP_SESSION_NONE) session_start();
-$_SESSION['user_id']=1;
-$_SESSION['last_order']='TEST123';
-
-include __DIR__ . '/tienda/confirmacion.php';
