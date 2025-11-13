13d727ff6cc1c7c4f28808aa67fc246a2d8801d0
Braian <braianoquen@gmail.com>
2025-11-13 08:42:32 -0500

    c

--- admin/subproducto.php
+++ admin/subproducto.php
@@ -309,9 +309,12 @@ if (isset($_SESSION['alert'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="base-url" content="<?= BASE_URL ?>">
+    <title>Agregar Producto - Panel de Administración</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/css/admin/style-admin.css">
-        <link rel="stylesheet" href="<?= BASE_URL ?>/css/dashboardadmin.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/dashboardadmin.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/admin/orders/orders.css">
     <link rel="stylesheet" href="<?= BASE_URL ?>/css/subproducto.css">
 </head>
 <body>
