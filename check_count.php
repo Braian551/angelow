5625a98776fdc688768c06f569476d800301f60e
Braian <braianoquen@gmail.com>
2025-11-26 12:20:01 -0500

    archivos

--- check_count.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/conexion.php';
-header('Content-Type: text/plain');
-$stmt = $conn->query("SELECT COUNT(*) as count FROM products WHERE collection_id = 1");
-$res = $stmt->fetch(PDO::FETCH_ASSOC);
-echo "Products in Collection 1 (Verano Mágico): " . $res['count'];
-?>
--- test_base_url.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config.php';
-echo 'BASE_URL: ' . BASE_URL . PHP_EOL;
-echo 'Current working directory: ' . getcwd() . PHP_EOL;
-echo 'Document root: ' . $_SERVER['DOCUMENT_ROOT'] . PHP_EOL;
-echo 'Script name: ' . $_SERVER['SCRIPT_NAME'] . PHP_EOL;
-?>
